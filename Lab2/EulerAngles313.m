bdcb650514a20132a85ec887dfcdb9504b4c82bd
Adarsh Boddeda <46360206+adarshb235@users.noreply.github.com>
2026-01-28 00:57:58 -0700

    Fix EulerAngles313 function calculations

--- Lab2/EulerAngles313.m
+++ Lab2/EulerAngles313.m
@@ -1,5 +1,5 @@
 function attitude313 = EulerAngles313(DCM)
     attitude313(2) = acos(DCM(3,3));
-    attitude313(1) = atan2(DCM(2,3), DCM(1,3));
-    attitude313(3) = atan2(DCM(3,2), -DCM(3,1));
+    attitude313(1) = atan2(DCM(1,3), DCM(2,3));
+    attitude313(3) = atan2(DCM(3,1), -DCM(3,2));
 end
