06e9dec0097f52e78bcf3838b5ef668e1e874497
Adarsh Boddeda <46360206+adarshb235@users.noreply.github.com>
2026-01-28 00:58:39 -0700

    Refactor Lab2Driver.m and enhance plot legends
    Refactored code to improve readability and added legends to plots.

--- Lab2/Lab2Driver.m
+++ Lab2/Lab2Driver.m
@@ -4,8 +4,8 @@ close all;
 
 
 filenames = ["3801_Sec002_Test1.csv" "3801_Sec002_Test2.csv" "3801_Sec002_Test3.csv"];
-for i = 1:1
-    data = readmatrix(filenames(i));
+for i = 1:length(filenames)
+    data = readmatrix(filenames(i)); % treat data
     data = rmmissing(data);
     data(:,2) = [];
 
@@ -14,10 +14,10 @@ for i = 1:1
     pos_tar_aspen = data(:,5:7)';
     att_tar_aspen = data(:,2:4)';
 
-    [t_vec, av_pos_inert, av_att, tar_pos_inert, tar_att] = LoadASPENData(filenames(i));
+    [t_vec, av_pos_inert, av_att, tar_pos_inert, tar_att] = LoadASPENData(filenames(i)); % read in E frame data
 
     % Problem 3
-    figure;
+    figure(i * 100 + 1);
     hold on;
     grid on;
     box on;
@@ -25,9 +25,10 @@ for i = 1:1
     plot3(pos_av_aspen(1, :), pos_av_aspen(2, :), pos_av_aspen(3, :), '-b');
     plot3(pos_tar_aspen(1, :), pos_tar_aspen(2, :), pos_tar_aspen(3, :), '--r');
     view(3)
+    legend('Aerospace Vehicle', 'Target', 'Location', 'best')
 
     % Problem 4.1
-    figure;
+    figure(i * 100 + 2);
     hold on;
     grid on;
     box on;
@@ -35,27 +36,34 @@ for i = 1:1
 
     subplot(3, 1, 1)
     hold on;
+    grid on;
     plot(t_vec, av_pos_inert(1, :), 'b');
     plot(t_vec, tar_pos_inert(1, :), 'r');
     ylabel('X Position (m)');
-    title('Position over Time');
+    title('X Position over Time');
+    legend('Aerospace Vehicle', 'Target', 'Location', 'best')
+
 
     subplot(3, 1, 2)
     hold on;
+    grid on;
     plot(t_vec, av_pos_inert(2, :), 'b');
     plot(t_vec, tar_pos_inert(2, :), 'r');
     ylabel('Y Position (m)');
-    title('Position vs Time');
+    title('Y Position vs Time');
+    legend('Aerospace Vehicle', 'Target', 'Location', 'best')
 
     subplot(3, 1, 3)
     hold on;
+    grid on;
     plot(t_vec, av_pos_inert(3, :), 'b');
     plot(t_vec, tar_pos_inert(3, :), 'r');
     ylabel('Z Position (m)');
-    title('Position vs Time');
+    title('Z Position vs Time');
+    legend('Aerospace Vehicle', 'Target', 'Location', 'best')
     
     % Problem 4.2
-    figure;
+    figure(i * 100 + 3);
     hold on;
     grid on;
     box on;
@@ -64,24 +72,30 @@ for i = 1:1
 
     subplot(3, 1, 1)
     hold on;
+    grid on;
     plot(t_vec, av_att(1, :), 'b');
     plot(t_vec, tar_att(1, :), 'r');
     ylabel('Pitch Angle (rad)');
     title('Average Pitch vs Time');
+    legend('Aerospace Vehicle', 'Target', 'Location', 'best')
 
     subplot(3, 1, 2)
     hold on;
+    grid on;
     plot(t_vec, av_att(2, :), 'b');
     plot(t_vec, tar_att(2, :), 'r');
     ylabel('Roll Angle (rad)');
     title('Average Roll vs Time');
+    legend('Aerospace Vehicle', 'Target', 'Location', 'best')
 
     subplot(3, 1, 3)
     hold on;
+    grid on;
     plot(t_vec, av_att(3, :), 'b');
     plot(t_vec, tar_att(3, :), 'r');
     ylabel('Yaw Angle (rad)');
     title('Average Yaw vs Time');
+    legend('Aerospace Vehicle', 'Target', 'Location', 'best')
 
     eulerAngles313_tar = zeros(3, length(att_tar_aspen));
     eulerAngles313_av = zeros(3, length(att_tar_aspen));
@@ -97,7 +111,7 @@ for i = 1:1
     end
 
     % Problem 5
-    figure;
+    figure(i * 100 + 4);
     hold on;
     grid on;
     box on;
@@ -106,51 +120,94 @@ for i = 1:1
 
     subplot(3, 1, 1)
     hold on;
+    grid on;
     plot(t_vec, eulerAngles313_av(1, :), 'b');
     plot(t_vec, eulerAngles313_tar(1, :), 'r');
     ylabel('Pitch Angle (rad)');
     title('Average Pitch vs Time');
+    legend('Aerospace Vehicle', 'Target', 'Location', 'best')
 
     subplot(3, 1, 2)
     hold on;
+    grid on;
     plot(t_vec, eulerAngles313_av(2, :), 'b');
     plot(t_vec, eulerAngles313_tar(2, :), 'r');
     ylabel('Roll Angle (rad)');
     title('Average Roll vs Time');
+    legend('Aerospace Vehicle', 'Target', 'Location', 'best')
 
     subplot(3, 1, 3)
     hold on;
+    grid on;
     plot(t_vec, eulerAngles313_av(3, :), 'b');
     plot(t_vec, eulerAngles313_tar(3, :), 'r');
     ylabel('Yaw Angle (rad)');
     title('Average Yaw vs Time');
+    legend('Aerospace Vehicle', 'Target', 'Location', 'best')
+
+    % Problem 6
+    rel_p_inert = tar_pos_inert - av_pos_inert;
+
+    figure(i * 100 + 5);
+    sgtitle("Problem 6: Components of Relative Position in Inertial Frame")
+
+    subplot(3, 1, 1)
+    hold on;
+    grid on;
+    title("X Position vs Time")
+    xlabel("Time (s)")
+    ylabel("X Position (m)")
+    plot(t_vec, rel_p_inert(1, :), 'k')
+
+    subplot(3, 1, 2)
+    hold on;
+    grid on;
+    title("Y Position vs Time")
+    xlabel("Time (s)")
+    ylabel("Y Position (m)")
+    plot(t_vec, rel_p_inert(2, :), 'k')
+
+    subplot(3, 1, 3)
+    hold on;
+    grid on;
+    title("Z Position vs Time")
+    xlabel("Time (s)")
+    ylabel("Z Position (m)")
+    plot(t_vec, rel_p_inert(3, :), 'k')
+
+    % Problem 7
+
+    rel_p_body = zeros(3, length(av_att));
+    for j = 1:length(av_att)
+        DCM = RotationMatrix321(av_att(:, j));
+        rel_p_body(:, j) = DCM * rel_p_inert(:, j);
+    end
+
+    figure(i * 100 + 6);
+    sgtitle("Problem 7: Components of Relative Position in Body Frame")
+
+    subplot(3, 1, 1)
+    hold on;
+    grid on;
+    title("X Position vs Time")
+    xlabel("Time (s)")
+    ylabel("X Position (m)")
+    plot(t_vec, rel_p_body(1, :), 'k')
+
+    subplot(3, 1, 2)
+    hold on;
+    grid on;
+    title("Y Position vs Time")
+    xlabel("Time (s)")
+    ylabel("Y Position (m)")
+    plot(t_vec, rel_p_body(2, :), 'k')
+
+    subplot(3, 1, 3)
+    hold on;
+    grid on;
+    title("Z Position vs Time")
+    xlabel("Time (s)")
+    ylabel("Z Position (m)")
+    plot(t_vec, rel_p_body(3, :), 'k')
+
 end
-
-% Problem 5
-
-pos_rel_E = tar_pos_inert - av_pos_inert;
-
-figure;
-hold on;
-grid on;
-box on;
-sgtitle("Problem 6: Relative Positions of Target With Respect to the Aerospace Vehicle in E Frame")
-
-
-subplot(3, 1, 1)
-hold on;
-plot(t_vec, pos_rel_E(1,:), 'b');
-ylabel('X position (m)');
-title('Relative X Position of Target With Respect to the Aerospace Vehicle in E Frame');
-
-subplot(3, 1, 2)
-hold on;
-plot(t_vec, pos_rel_E(2,:), 'b');
-ylabel('Y position (m)');
-title('Relative Y Position of Target With Respect to the Aerospace Vehicle in E Frame');
-
-subplot(3, 1, 3)
-hold on;
-plot(t_vec, pos_rel_E(3,:), 'b');
-ylabel('Z position (m)');
-title('Relative Z Position of Target With Respect to the Aerospace Vehicle in E Frame');
