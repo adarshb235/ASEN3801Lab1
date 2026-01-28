d8775c5542289191da8936d9ffec34e0ca18c6b9
Adarsh Boddeda <46360206+adarshb235@users.noreply.github.com>
2026-01-28 00:58:18 -0700

    Document EulerAngles321 function
    Added function documentation for EulerAngles321.

--- Lab2/EulerAngles321.m
+++ Lab2/EulerAngles321.m
@@ -1,4 +1,8 @@
 function attitude321 = EulerAngles321(DCM)
+% EulerAngles321 - extracts euler angles from 3-2-1 DCM
+% attitude321 = EulerAngles321(DCM)
+% Inputs: DCM = 3x3 matrix for 
+%
     attitude321(1) = atan2(DCM(2,3), DCM(3,3));
     attitude321(2) = asin(-DCM(1, 3));
     attitude321(3) = atan2(DCM(1,2), DCM(1,1));
